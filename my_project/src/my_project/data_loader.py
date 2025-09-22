4c784d90a1a88424090a22cf4e3e1ec19ad3214d
selivanek <isabriella@gmail.com>
2025-09-23 00:09:41 +0300

    Homework_2

---
+++ my_project/src/my_project/data_loader.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+FILE_ID = "1II7XwNM6qMuOzXXSQhrmXQjF3tth7NPh"
+file_url = f"https://drive.google.com/uc?id={FILE_ID}"
+
+raw_data = pd.read_csv(file_url)
+
+print(raw_data.head(10))
